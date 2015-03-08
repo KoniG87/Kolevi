96d8019028fe3951d582edc88bfbe52cbb03ae50
Gabor <gabor.gv@gmail.com>
2015-03-08 10:11:53 +0100

    Removed sallang. :)

--- index.php
+++ index.php
@@ -8,25 +8,5 @@
     if (file_exists('page/'.$page.'.php')){
         include('page/'.$page.'.php');
     }
-    /*
-    if (!$app->validView($page)){
-        header("HTTP/1.0 404 Not Found");
-        $page = '404';
-    }
-    
-    if (!isset($_POST['formSubmitted']) && $page != 'process' && !isset($_POST['processIndicator'])){
-    //if (!isset($_POST['formSubmitted']) && $page != 'process' && $page != 'home' && !isset($_POST['processIndicator'])){
-        $app->generateHtmlFrame('header');
-		$app->loadDependencies($defaultDependencies);
-        $app->generateHtmlFrame('menu');
-    }
-    
-    $app->generateView($page); 
-    
-    
-    if (!isset($_POST['formSubmitted']) && $page != 'process' && !isset($_POST['processIndicator'])){
-    //if (!isset($_POST['formSubmitted']) && $page != 'process' && $page != 'home' && !isset($_POST['processIndicator'])){
-        $app->generateHtmlFrame('footer');
-    }
-    */
+   
 ?>
