10cd3bf1897913e7c0c02908a1d4d8e6cf090a83
Gabor <Gabor@192.168.2.21>
2015-05-12 20:38:24 +0200

    PDF footer belőve

--- assets/libs/tcpdf/kolevespdf.php
+++ assets/libs/tcpdf/kolevespdf.php
@@ -2,15 +2,23 @@
 
 class kolevesPDF extends TCPDF{
 	public function Footer() {
-		$footer = '<hr style="width:98%;height:4px;border-color:#6d6d6d;"/>
-			<p>
-			Szervízdíjat nem számolunk fel, ezért ha elégedettek voltatok, <br/>
+		$this->SetY(-47);
+		$this->SetFont('freesans', '', 8);
+		
+		$footer = '
+			<p style="text-align:center;">
+			
+			
+				Szervízdíjat nem számolunk fel, ezért ha elégedettek voltatok, <br/>
 			a borravalót a pincéreknek adjátok oda! Köszönjük!<br/><br/>
 			
 			Kreatív chef: Bezdán Anita • Chef: Bíró Dániel • Üzletvezető: Dyssou Bona • Vendéglősök: Elek Imola, Kápolnai Gábor Zebulon<br/>
-			Szeressétek: www.facebook.com/koleves Véleményeteket írjátok meg a www.tripadvisor.com/koleves oldalon<br/><br/>
+			
+			Szeressétek: <strong>www.facebook.com/koleves</strong> 
+			Véleményeteket írjátok meg a <strong>www.tripadvisor.com/koleves</strong> oldalon<br/><br/>
 
-			www.koleves.com
+			<strong>www.koleves.com</strong>
+			
 		</p>';
 		$this->writeHTML($footer, true, false, true, false, '');
 		
