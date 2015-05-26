2e82730b20908966b5993e82fd16a766c076734c
Koni <gergely.konradg@gamil.com>
2015-05-26 16:03:04 +0200

    étlep piktók kész

--- core/template/etlapForma.php
+++ core/template/etlapForma.php
@@ -1,5 +1,43 @@
 <!-- <p>[[MEGNEVEZES]] • [[TAGEK]]<span> — [[AR]] Ft</span></p> -->
-<tr class="etlap-sor">
+<!-- <tr class="etlap-sor">
 <td>[[MEGNEVEZES]] • [[TAGEK]]</td>
 <td>[[AR]] Ft</td>
+</tr> -->
+
+<tr class="etlap-sor">
+<td>[[MEGNEVEZES]] • 
+<span class="allergen alg-1"></span>
+<span class="allergen alg-2"></span>
+<span class="allergen alg-3"></span>
+<span class="allergen alg-4"></span>
+<span class="allergen alg-5"></span>
+<span class="allergen alg-6"></span>
+<span class="allergen alg-7"></span>
+<span class="allergen alg-8"></span>
+<span class="allergen alg-9"></span>
+<span class="allergen alg-10"></span>
+<span class="allergen alg-11"></span>
+<span class="allergen alg-12"></span>
+<span class="allergen alg-13"></span>
+<span class="allergen alg-14"></span>
+</td>
+<td>[[AR]] Ft</td>
 </tr>
+
+
+<!-- 
+alg-1 -- csillagfürt
+alg-2 -- diófélék
+alg-3 -- földimogyi
+alg-4 -- glutén
+alg-5 -- hal
+alg-6 -- mustár
+alg-7 -- puhatestűek
+alg-8 -- rákfélék
+alg-9 -- szezámmag
+alg-10 -- szójabab
+alg-11 -- szulfitok
+alg-12 -- tej
+alg-13 -- tojás
+alg-14 -- zeller
+ -->
